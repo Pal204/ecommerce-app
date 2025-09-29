451d0d6d18205009e3a471b08c8f0f7602290470
Pal204 <vanshchavda111@gmail.com>
2025-09-29 20:14:45 +0530

    Created collection page,searchbar,added filters

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-30 text-sm'>
+                <div>
+                    <img src={assets.logo} alt="" className='mb-5 w-32' />
+                    <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ratione libero placeat optio quidem aspernatur reprehenderit saepe, ab perspiciatis, quae, repellat quibusdam officiis natus!</p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-1 text-gray-600 cursor-pointer '>
+                        <li className='hover:text-black'>Home</li>
+                        <li className='hover:text-black'>About</li>
+                        <li className='hover:text-black'>Delivery</li>
+                        <li className='hover:text-black'>Privacy policy</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>+91-1234-587-925</li>
+                        <li>contact@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2025@ forevr.com - All Rights Reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
