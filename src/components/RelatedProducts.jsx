50754ca0cfe6622beb06fda0e4890bfd95844587
Pal204 <vanshchavda111@gmail.com>
2025-09-30 20:21:16 +0530

    Created related & bestseller product component & cart component

---
+++ src/components/RelatedProducts.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopConext } from '../context/ShopContext'
+import Title from './Title';
+import PrdouctItems from './PrdouctItems';
+
+const RelatedProducts = ({ category, subCategory }) => {
+
+    const { products } = useContext(ShopConext);
+    const [related, setRelated] = useState([]);
+
+    useEffect(() => {
+        if (products.length > 0) {
+            let productsCopy = products.slice();
+            productsCopy = productsCopy.filter((item) => category === item.category);
+            productsCopy = productsCopy.filter((item) => subCategory === item.subCategory);
+            setRelated(productsCopy.slice(0, 5));
+        }
+
+    }, [products])
+
+    return (
+        <div className='my-24'>
+            <div className='text-center text-3xl py-2'>
+                <Title text1={'RELATED'} text2={'PRODUCTS'} />
+            </div>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {
+                    related.map((item, index) => (
+                        <PrdouctItems key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default RelatedProducts
